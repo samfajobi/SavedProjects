7c358cfbdb18ba4d18b439752b04a097f9766cd2
Fajobi Olusegun <segzyfaj1@gmail.com>
2024-05-03 12:00:19 +0100

    First commit

---
+++ ProjectPractice/Web3Practice/Hello.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+// Constant and Immutable in Solidity
+
+
+
+
+
+
+
+
+
+
+
+
+// mapping(address => uint) public balances 
+// balances[msg.sender] = 123;  --- (set data)
+// uint bal = balances[msg.sender];  --- (get data)
+// uint bal2 = balances[address(1)]; // 0
+// balances[msg.sender] += 456  --- (update data)
+// delete balances[msg.sender];  --- (delete data)
+
+
+
+//Struct
+//State variables
+//Constructor
+//Mapping
+//Modifier
+//Events
+//Enum
+
+
+
+//   create a new repository on the command line
+// echo "# staking-contract" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/samfajobi/staking-contract.git
+// git push -u origin main
+
+
+//    push an existing repository from the command line
+// git remote add origin https://github.com/samfajobi/staking-contract.git
+// git branch -M main
+// git push -u origin main
---
+++ ProjectPractice/Web3Practice/Hello.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT;
+
+
+
+contract practice {
+    
+    // min and max is used to prevent underflow and overflow.
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+}
+
+
+contract deployContract {
+    function run() external returns(sim)
+}
